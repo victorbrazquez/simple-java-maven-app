4ed39beabe7f5df568c2e002b1218396f5976f64
victorbrazquez <63924912+victorbrazquez@users.noreply.github.com>
2025-01-15 16:08:20 +0100

    Update javamavenDSL3.groovy

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 3') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/victorbrazquez/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('victorbrazquez')
+            node / gitConfigEmail('vmbv21@gmail.com')
         }
     }
     triggers {
@@ -49,9 +49,9 @@ job('Java Maven App DSL 3') {
 job('Job test Hola Mundo') {
 	description('Aplicacion Hola Mundo de Prueba')
 	scm {
-		git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-		    node / gitConfigName('macloujulian')
-		    node / gitConfigEmail('macloujulian@gmail.com')
+		git('https://github.com/victorbrazquez/simple-java-maven-app.git', 'master') { node ->
+		    node / gitConfigName('victorbrazquez')
+		    node / gitConfigEmail('vmbv21@gmail.com')
 		}
 	}
 	triggers {
